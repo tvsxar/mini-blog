446f89260746e28590a95bd81e65509751309272
Taras Poiatsyka <tvsxar@MacBook-Air---Taras.local>
2025-11-13 14:46:22 +0200

    Error handling

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,11 +4,17 @@ import { PostsContext } from '../context/PostsContext';
 
 function HomePage() {
   // Context
-  const { posts } = useContext(PostsContext);
+  const { posts, postError } = useContext(PostsContext);
 
   return (
     <div className='px-4 sm:px-12 lg:px-25 xl:px-25'>
       <div className="flex flex-col gap-16 sm:gap-16 py-8 sm:py-12">
+        {postError && (
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded mb-4">
+            {postError}
+          </div>
+        )}
+
         {posts.map(post => <PostItem key={post._id} post={post} />)}
       </div>
     </div>
