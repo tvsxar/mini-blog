8cf9cfed47549b739526de67e8acce8c229f8cee
Taras Poiatsyka <tvsxar@MacBook-Air---Taras.local>
2025-11-18 21:16:35 +0200

    Changes in backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use('/api/posts', postRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get('*', (req, res) => {
+  app.get('/{*splat}', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
   })
 } else {
